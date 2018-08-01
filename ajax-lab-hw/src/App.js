a9997f0dd679002cefd42f95216552ea17f5eee6
-thugnasty- <eterictodd@gmail.com>
2018-07-31 22:15:06 -0600

    basic pseudo login created.

--- ajax-lab-hw/src/App.js
+++ ajax-lab-hw/src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./Login";
+import MainContainer from "./MainContainer";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      username: ""
+    }
+  }
+
+  login = (username) => {
+    console.log("Login function working.", username);
+    this.setState({
+      username: username,
+      loggedIn: true
+    });
+  }
+
   render() {
+    console.log(this.state);
+    let loggedIn = this.state.loggedIn;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.loggedIn ? <MainContainer username={this.state.username}/> : <Login login={this.login}/>}
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ ajax-lab-hw/src/Login/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.login(this.state.username);
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="username" onChange={this.handleChange} value={this.state.username} placeholder="username" />
+                <input type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="password" />
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
+};
+
+
+export default Login;
